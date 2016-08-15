403baa8a12f2218540464d0fb5990bc21bc951ba
Alex Alspach <alexalspach@gmail.com>
2016-08-15 01:44:14 -0400

    added papers, fixed formatting, uploaded photos to be added to projects page

---
+++ style/theme_js/include_html_stage.js
@@ -0,0 +1,41 @@
+$(function(){
+  $("#includeMe").load("me.html"); 
+});
+
+$(function(){
+  $("#includeContact").load("contact.html"); 
+});
+
+$(function(){
+  $("#includeEducation").load("education.html"); 
+});
+
+$(function(){
+  $("#includeExperience").load("experience.html"); 
+});
+
+$(function(){
+  $("#includePapers").load("paperspatents.html"); 
+});
+
+$(function(){
+  $("#includePresentations").load("presentations.html"); 
+});
+
+//name of page may change 
+$(function(){
+  $("#includeProjects").load("projects_stage.html"); 
+});   
+
+$(function(){
+  $("#includeVideos").load("videos.html"); 
+});
+
+$(function(){
+  $("#includeSmallWorld").load("smallWorld.html"); 
+});
+
+$(function(){
+  $("#includeStage").load("stage.html"); 
+});	
+
